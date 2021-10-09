a8bf082652c22037b3ae8c2ac0db793682f8fbb6
HemanthGK-GK <hemanthgk804@gmail.com>
2021-10-09 16:24:03 +0530

    Hemanth : Remove specific word using key Value

---
+++ UC3Remove/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace UC3Remove
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyMap<string, string> hash = new MyMap<string, string>(18);
+            hash.Add("0", "Paranoids");
+            hash.Add("1", "are");
+            hash.Add("2", "not");
+            hash.Add("3", "paranoid");
+            hash.Add("4", "because");
+            hash.Add("5", "they");
+            hash.Add("6", "are");
+            hash.Add("7", "paranoid");
+            hash.Add("8", "but");
+            hash.Add("9", "because");
+            hash.Add("10", "they");
+            hash.Add("11", "keep");
+            hash.Add("12", "putting");
+            hash.Add("13", "themselves");
+            hash.Add("14", "deliberately");
+            hash.Add("15", "into");
+            hash.Add("16", "paranoid");
+            hash.Add("17", "avoidable");
+            hash.Add("18", "situations");
+
+            Console.WriteLine("Removing the word(avoidable)");
+            hash.Remove("17");
+
+        }
+    }
+}
