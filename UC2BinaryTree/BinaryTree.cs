de4dc75028d568a8d621b5534ce0a4a79d32596d
HemanthGK-GK <hemanthgk804@gmail.com>
2021-10-09 16:28:44 +0530

    Hemanth :Inserting new node to the Tree

---
+++ UC2BinaryTree/BinaryTree.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC2BinaryTree
+{
+
+
+    class BinaryTree
+    {
+
+        public Node Root { get; set; }
+
+        public bool Add(int value)
+        {
+            Node before = null, after = this.Root;
+
+            while (after != null)
+            {
+                before = after;
+                if (value < after.Data)
+                    after = after.LeftNode;
+                else if (value > after.Data)
+                    after = after.RightNode;
+                else
+                {
+                    return false;
+                }
+            }
+
+            Node newNode = new Node();
+            newNode.Data = value;
+            if (this.Root == null)
+            {
+                this.Root = newNode;
+            }
+            else
+            {
+                if (value < before.Data)
+                    before.LeftNode = newNode;
+                else
+                    before.RightNode = newNode;
+            }
+            return true;
+        }
+        public void TravarseInorder(Node Parent)
+        {
+            if (Parent != null)
+            {
+                TravarseInorder(Parent.LeftNode);
+                Console.Write(Parent.Data + " ");
+                TravarseInorder(Parent.RightNode);
+
+            }
+
+        }
+    }
+
+}
---
+++ UC2BinaryTree/Node.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC2BinaryTree
+{
+    class Node
+    {
+        public Node LeftNode { get; set; }
+        public Node RightNode { get; set; }
+        public int Data { get; set; }
+    }
+
+   
+ 
+}
